46e6eb11cb48c49df3ee4d10845844cbf7dbfb55
root <root@ip-172-31-13-156.ap-northeast-2.compute.internal>
2017-12-17 09:02:38 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import Header from './Header/Header';
+import Content from './Content/Content';
+import styles from './App.css';
+import sassStyles from './App.scss';
 
 class App extends React.Component {
     render(){
 
         return (
-                <h1>Hello React Skeleton</h1>
+ 	      <div className={styles.container}>
+                <Header/>
+		<div>
+		   <div className={sassStyles.box}>BOX</div>
+		   <div className={styles.hello}>
+			Hello
+		   </div>
+		   <Content/>
+		
+		</div>
+	      </div>
         );
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV === "development"
+});
+
+
 module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: __dirname + '/public',
+        path: __dirname + '/public/bundle',
         filename: 'bundle.js'
     },
 
@@ -23,7 +31,23 @@ module.exports = {
                     cacheDirectory: true,
                     presets: ['es2015', 'stage-0', 'react']
                 }
+            },
+	    {
+             test: /\.css$/,
+             loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+            },
+            {
+             test: /\.scss$/,
+             use: extractSass.extract({
+                use: ['css-loader','sass-loader'],
+                // use style-loader in development
+                fallback: "style-loader"
+              })
             }
-        ]
-    }
+ 
+          ]
+    },
+      plugins : [
+	extractSass
+      ]
 };
